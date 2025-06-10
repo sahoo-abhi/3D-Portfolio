761a269b42ef1bee1ee5cde92738edf5230f83a0
Abhishek <abhisheksahoo788@gmail.com>
2025-06-10 16:03:16 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,152 @@
+//import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    //redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+   
+   /* {
+        iconUrl: ,
+        companyName: '',
+        role: '',
+        duration: '',
+        description: '',
+    },*/
+    
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/sahoo-abhi',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/abhishek-sahoo-938a692b5/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'MovieHub',
+        description: 'Developed a fully functional movie streaming website using modern frontend technologies and real-time data integration.',
+        link: 'https://github.com/sahoo-abhi/Movie-Streaming-Website',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-blue',
+        name: 'Malicious URL Detector',
+        description: 'Created a machine learning model to detect malicious URLs, enhancing cybersecurity measures.',
+        link: ''
+    }
+];
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+
+import { CTA } from "../components";
+import { projects } from "../constants";
+import { arrow } from "../assets/icons";
+
+const Projects = () => {
+  return (
+    <section className='max-container'>
+     <div className="max-w-4xl mx-auto px-4">
+        <h1 className="text-4xl font-bold">
+          My{" "}
+           <span className="text-blue-600 drop-shadow font-semibold">
+             Projects
+           </span>
+        </h1> 
+        <p className="mt-4 text-justify text-gray-700 leading-relaxed">
+            Developed and deployed multiple end-to-end software applications demonstrating strong proficiency in full-stack development and machine learning. Projects involved designing scalable system architectures, implementing responsive user interfaces, and integrating backend services and APIs. Applied best practices in version control, testing, and deployment to ensure high-quality deliverables. Focused on solving real-world problems using efficient and maintainable code.
+        </p>
+     </div>
+
+
+      <div className='flex flex-wrap my-20 gap-16'>
+        {projects.map((project) => (
+          <div className='lg:w-[400px] w-full' key={project.name}>
+            <div className='block-container w-12 h-12'>
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={project.iconUrl}
+                  alt='threads'
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+
+            <div className='mt-5 flex flex-col'>
+              <h4 className='text-2xl font-poppins font-semibold'>
+                {project.name}
+              </h4>
+              <p className='mt-2 text-slate-500'>{project.description}</p>
+              <div className='mt-5 flex items-center gap-2 font-poppins'>
+                <Link
+                  to={project.link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='font-semibold text-blue-600'
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt='arrow'
+                  className='w-4 h-4 object-contain'
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default Projects;
